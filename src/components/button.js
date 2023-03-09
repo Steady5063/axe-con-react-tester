f4a43a4d08737ffff072c995f4723366e0360940
Mark Edward Steadman <47225088+Steady5063@users.noreply.github.com>
2023-03-09 08:40:57 -0600

    add: Helper Text to button

--- src/components/button.js
+++ src/components/button.js
@@ -7,7 +7,10 @@ class SaberButton extends React.Component {
   //TODO: ADD CSS FOR DIFFERENT BUTTON TYPES
     render() {
       return (    
+        <div>
         <button className={this.props.buttonType}>{this.props.buttonText}</button>
+        <span>Helper text</span>
+        </div>
       );
     }
   }
